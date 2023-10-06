7ff5c656a7b58d6c747d0f129d80b08a2bccc994
Tawnee <allprt89@gmail.com>
2023-10-05 20:06:04 -0600

    use module. get char count working

--- bin/index.js
+++ bin/index.js
@@ -1,10 +1,17 @@
 #! /usr/bin/env node
-const yargs = require("yargs");
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { readFile } from "node:fs/promises";
+
+const requireFile = async (path) => {
+  const data = await readFile(path);
+  return data.length;
+};
 
 const usage =
   "\nThe nodewc utility displays the number of lines, words, and bytes contained in each input file, or standard input (if no file is specified) to the standard output.";
 
-const options = yargs
+const options = yargs(hideBin(process.argv))
   .usage(usage)
   .option("c", {
     describe: "The number of bytes in each input file.",
@@ -12,3 +19,17 @@ const options = yargs
     demandOption: false,
   })
   .help(true).argv;
+
+if (options.c) {
+  try {
+    const filePaths = options._;
+    const filePromises = filePaths.map((file) => requireFile(file));
+    const charCounts = await Promise.all(filePromises);
+
+    charCounts.forEach((count, index) => {
+      console.log(count, filePaths[index]);
+    });
+  } catch (e) {
+    console.error(e.message);
+  }
+}
